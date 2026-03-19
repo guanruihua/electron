e74fbbb29bc798da6deef66c03fc3edefda5bc21
guanruihua <2116047353@qq.com>
2026-03-19 16:23:00 +0800

    feat: feat: Dev

--- src/renderer/src/layout/home-view/modules/info/info.tsx
+++ src/renderer/src/layout/home-view/modules/info/info.tsx
@@ -2,29 +2,39 @@ import { ModuleProps } from '@/layout/type'
 import dayjs from 'dayjs'
 import React from 'react'
 
-function updateCountdown() {
-  const now = dayjs()
-  let target = dayjs().hour(18).minute(0).second(0).millisecond(0)
-  if (now.isAfter(target)) {
-    target = target.add(1, 'day')
-  }
-
-  const diffMs = target.diff(now)
-  if (diffMs <= 0) {
-    return '下班啦！'
-  }
-
-  const diffSeconds = Math.floor(diffMs / 1000)
-  const hours = Math.floor(diffSeconds / 3600)
-  const minutes = Math.floor((diffSeconds % 3600) / 60)
-  const seconds = diffSeconds % 60
-  return `距离下班还有：${hours}小时 ${minutes}分钟 ${seconds}秒`
-}
-
 export function Info(props: ModuleProps) {
   const {} = props
-  const [ddl, setDDL] = React.useState('今天不用上班')
+  const [ddl, setDDL] = React.useState('今天不用上班！')
   const timer = React.useRef<NodeJS.Timeout | null>(null)
+  function updateCountdown() {
+    const now = dayjs()
+    if ([0, 6].includes(now.day())) {
+      if (timer.current) clearInterval(timer.current)
+      return '今天不用上班！'
+    }
+    let target = dayjs().hour(18).minute(0).second(0).millisecond(0)
+    if (now.isAfter(target)) {
+      target = target.add(1, 'day')
+    }
+
+    const diffMs = target.diff(now)
+    if (diffMs <= 0) {
+      if (timer.current) clearInterval(timer.current)
+      return '下班啦！'
+    }
+
+    const diffSeconds = Math.floor(diffMs / 1000)
+    const hours = Math.floor(diffSeconds / 3600)
+    const minutes = Math.floor((diffSeconds % 3600) / 60)
+    const seconds = diffSeconds % 60
+    if (hours < 1) {
+      return `距离下班还有：${minutes}分钟 ${seconds}秒`
+    }
+    if (hours<1 && minutes < 1) {
+      return `距离下班还有：${seconds}秒`
+    }
+    return `距离下班还有：${hours}小时 ${minutes}分钟 ${seconds}秒`
+  }
   React.useEffect(() => {
     setDDL(updateCountdown())
 
@@ -36,6 +46,7 @@ export function Info(props: ModuleProps) {
       timer.current && clearInterval(timer.current)
     }
   }, [])
+
   return (
     <div className="root-layout-home-view-info">
       <div className="module-bg">

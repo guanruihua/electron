4e36049b0e13882592bf9751df0ae0bd315e4e72
guanruihua <2116047353@qq.com>
2026-05-18 10:09:15 +0800

    feat: dev

--- src/renderer/src/views/dashboard/index.tsx
+++ src/renderer/src/views/dashboard/index.tsx
@@ -15,17 +15,17 @@ export default function DashboardView() {
         <div className="dashboard-layout-col">
           <Info />
           <QuickStart />
+          <RunningApp />
         </div>
         <div className="dashboard-layout-col">
           <Dash_Project />
         </div>
         <div className="dashboard-layout-col">
           <ProjectOperation />
+          <GitReview />
           <NodeTread />
-          <RunningApp />
         </div>
         <div className="flex gap col">
-          <GitReview />
           <Dash_Timeline />
         </div>
       </div>
